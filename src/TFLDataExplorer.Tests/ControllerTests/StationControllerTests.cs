aa5988df1bfc1be1e4bd7201f4e82bea8aea198d
Tim <Tim@TIM-PC>
2017-05-06 08:16:36 +0100

    Added Unit Test for StationController

---
+++ src/TFLDataExplorer.Tests/ControllerTests/StationControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TFLDataExplorer.Controllers;
+using Microsoft.Extensions.Options;
+using TFLDataExplorer.Models;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TFLDataExplorer.Tests
+{
+    public class StationControllerTests
+    {
+
+        [Fact]
+        public async Task GetStationsByZone_Action_Returns_Stations_With_Selected_Zone_List()
+        {
+            // Arrange.
+            string zoneId = "3";
+
+            // Create some fake stations in different Zones.
+            var fakeStationOne = new Station();
+            fakeStationOne.Name = "Fake Station One";
+            fakeStationOne.Zones = new Zones { Zone = "1" };
+
+            var fakeStationTwo = new Station();
+            fakeStationTwo.Name = "Fake Station Two";
+            fakeStationTwo.Zones = new Zones { Zone = "2" };
+
+            var fakeStationThree = new Station();
+            fakeStationThree.Name = "Fake Station Three";
+            fakeStationThree.Zones = new Zones { Zone = "3" };
+
+            var fakeStationsModel = new StationsModel();
+            var fakeStations = new Stations();
+            var stationList = new List<Station>();
+
+            fakeStationsModel.Stations = fakeStations;
+            fakeStationsModel.Stations.Station = stationList;
+            
+            fakeStations.Station.Add(fakeStationOne);
+            fakeStations.Station.Add(fakeStationTwo);
+            fakeStations.Station.Add(fakeStationThree);
+            
+            // Create some mock options to avoid Null Reference Exception.
+            MyOptions options = new MyOptions() { AppId = "Foo", AppKey = "Bar" };
+
+            var mockOptions = new Mock<IOptions<MyOptions>>();
+            mockOptions.Setup(x => x.Value)
+                .Returns(options);
+
+            // Mock the API caller to return the details of Stations.
+            var mockStationServiceAsync = new Mock<IStationService>();
+            mockStationServiceAsync.Setup(x => x.GetStationsAsync(It.IsAny<string>()))
+               .ReturnsAsync(fakeStationsModel);
+
+            // Act.
+            StationController stationController = new StationController(mockOptions.Object, mockStationServiceAsync.Object);
+            var actionResult = await stationController.GetStationsByZone(zoneId);
+
+            var jsonResult = actionResult as JsonResult;
+
+            var result = jsonResult.Value as SelectList;
+
+
+            // Assert.
+            Assert.NotNull(jsonResult);
+            Assert.IsType<SelectList>(jsonResult.Value);
+            Assert.NotNull(result);
+            Assert.Equal("Fake Station Three", result.FirstOrDefault().Text);
+                     
+
+        }
+
+    }
+}
